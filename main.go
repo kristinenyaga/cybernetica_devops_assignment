4857e1d1e1dcb930a18bc3229757bca8828d5d19
christinenyagah <kristinenyaga@gmail.com>
2026-03-31 12:27:01 +0300

    refactor: add environment-based configuration

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"net/http"
+	"os"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Write([]byte("Service request succeeded!"))
-}
-
 func main() {
-	http.HandleFunc("/", helloWorld)
-	http.ListenAndServe(":8000", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	responseMessage := os.Getenv("RESPONSE_MESSAGE")
+	if responseMessage == "" {
+		responseMessage = "Service request succeeded!"
+	}
+
+	allowOrigin := os.Getenv("ALLOW_ORIGIN")
+	if allowOrigin == "" {
+		allowOrigin = "*"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+		w.Write([]byte(responseMessage))
+	})
+
+	http.ListenAndServe(":"+port, nil)
 }
